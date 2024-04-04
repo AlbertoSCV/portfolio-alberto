77c1df250842e96530a08bb15c0d10f96307e792
AlbertoSCV <albertos@somoza-labs.com>
2024-04-04 16:09:58 -0500

    Fix z-index Left Text

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="footer border border-t-[#33353F] border-b-transparent border-l-transparent border-r-transparent">
-        <div className='z-10 text-xs container py-12 px-5 sm:py-12 sm:px-12 sm:text-base flex justify-between'>
-            <span>
+        <div className='text-xs container py-12 px-5 sm:py-12 sm:px-12 sm:text-base flex justify-between'>
+            <span className='z-50'>
                 Alberto Somoza™
             </span>
             <p className='text-slate-400'>
