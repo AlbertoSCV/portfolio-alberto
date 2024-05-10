f52ecec5c11f34f28021859ed37561d04100c54e
AlbertoSCV <albertos@somoza-labs.com>
2024-05-10 07:02:20 +0000

    Add V Title

--- app/cv/components/See_CV.jsx
+++ app/cv/components/See_CV.jsx
@@ -19,6 +19,9 @@ const See_CV = () => {
     transition={{duration: 0.7, delay:0.2}} 
     className='mt-10'>
         <div>
+            <h1 className='ml-8 font-extrabold text-6xl my-8'>
+                Curriculum Vitae
+            </h1>
             <div className='hidden sm:flex sm:flex-row sm:justify-end'>
                 <Link href='/cv_es.pdf' download="cv" className='mr-6 absolute sm:mt-5 md:mt-4'>
                     <button href='/cv' className="font-semibold px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-tr from-[#FDA000] to-[#D3208B] text-white hover:bg-slate-200">
